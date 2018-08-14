9eb53e306358fd0c5e628a40b27270b12195c0ee
islab <msps50807@gmail.com>
2018-08-14 15:52:17 +0800

    fix

--- project1/authentication.py
+++ project1/authentication.py
@@ -8,5 +8,5 @@ def login(username, password):
         users = [line.split("|") for line in user_buf.split("\n")]
         return [username, password] in users
     except IOError:
-        print "I can't authentication you."
+        print("I can't authentication you.")
         return False
