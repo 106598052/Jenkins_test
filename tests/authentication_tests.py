46f6039adcb4f2f6bbc1f2d8aadcde931bbfbfc0
islab <msps50807@gmail.com>
2018-08-14 16:16:27 +0800

    fix

--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -7,18 +7,16 @@ import project1.authentication as auth
 
 class StandAloneTests(TestCase):
     """Test the stand-alone module functions."""
-
     @patch('__builtin__.open')
     def test_login_success(self, mock_open):
         """Test the login function when things go right."""
-        print("Test")
         mock_open.return_value.read.return_value = \
             "george|bosco"
-        self.assertTrue(auth.login('george', 'bosco'))
+            self.assertTrue(auth.login('george', 'bosco'))
 
     @patch('__builtin__.open')
     def test_login_bad_creds(self, mock_open):
-		"""Test the login function when bad creds are passed."""
+        """Test the login function when bad creds are passed."""
         mock_open.return_value.read.return_value = \
             "george|bosco"
         self.assertFalse(auth.login('george', 'marbleRye'))
@@ -27,6 +25,4 @@ class StandAloneTests(TestCase):
     def test_login_error(self, mock_open):
         """Test the login function when an error happens."""
         mock_open.side_effect = IOError()
-        self.assertFalse(auth.login('george', 'bosco'))
-
-        
+        self.assertFalse(auth.login('george', 'bosco')) 
